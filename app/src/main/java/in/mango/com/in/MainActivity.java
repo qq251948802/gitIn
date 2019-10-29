0c721cfb3a50528e492c10fe382b28cb742bc026
caizf <windows_long@126.com>
2019-10-29 17:38:11 +0800

    gradle各项说明和多渠道配置

--- app/src/main/java/in/mango/com/in/MainActivity.java
+++ app/src/main/java/in/mango/com/in/MainActivity.java
@@ -1,13 +1,26 @@
 package in.mango.com.in;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        try {
+            PackageManager pm = this.getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+            String channel=appInfo.metaData.getString("APP_CHANNEL_KEY");
+            Toast.makeText(this,channel+"=="+BuildConfig.APP_TYPE,Toast.LENGTH_LONG).show();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
